9de71cdb844dee5b11289c358e50e3ae532c890d
minhthuong031103 <nominh67@gmail.com>
2023-08-29 23:31:46 +0700

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
-import classNames from 'classnames';
 import { CaretDownIcon } from '@radix-ui/react-icons';
 import './styles.css';
 
@@ -117,14 +116,10 @@ const NavigationMenuDemo = () => {
 };
 
 const ListItem = React.forwardRef(
-  ({ className, children, title, ...props }, forwardedRef) => (
+  ({ children, title, ...props }, forwardedRef) => (
     <li>
       <NavigationMenu.Link asChild>
-        <a
-          className={classNames('ListItemLink', className)}
-          {...props}
-          ref={forwardedRef}
-        >
+        <a className={'ListItemLink'} {...props} ref={forwardedRef}>
           <div className="ListItemHeading">{title}</div>
           <p className="ListItemText">{children}</p>
         </a>
