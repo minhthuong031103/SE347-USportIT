f1680eb51e695d6af6ce821f0cf369285585913f
MinhQuan020903 <doquan020903@gmail.com>
2023-10-06 17:28:37 +0700

    Complete ReviewForm
    Add login requied when writing review.
    
    Add login required when writing review.
    
    Add login required when writing review
    
    fix
    
    fix
    
    Done form review
    
    up

--- src/app/(client)/(withHDFT)/product/[slug]/ProductReviewItem.tsx
+++ src/app/(client)/(withHDFT)/product/[slug]/ProductReviewItem.tsx
@@ -1,4 +1,8 @@
 'use client';
+<<<<<<< HEAD:src/app/(client)/(withHDFT)/product/[slug]/ProductReviewItem.tsx
+<<<<<<<< HEAD:src/app/(client)/(withHDFT)/product/[slug]/ProductReviewItem.tsx
+=======
+>>>>>>> d5e50d7 (Complete ReviewForm):src/app/(client)/product/[slug]/ProductReviewItem.tsx
 import ReviewDetail from '@/components/ReviewDetail';
 
 import React from 'react';
@@ -35,7 +39,11 @@ const ProductReviewItem = ({
                 </div>
               ))}
         </div>
+<<<<<<< HEAD:src/app/(client)/(withHDFT)/product/[slug]/ProductReviewItem.tsx
         {reviewItemData ? (
+=======
+        {reviewItemData?.data?.length == 0 && data != null ? (
+>>>>>>> d5e50d7 (Complete ReviewForm):src/app/(client)/product/[slug]/ProductReviewItem.tsx
           <div></div>
         ) : (
           <Pagination
@@ -48,6 +56,83 @@ const ProductReviewItem = ({
             page={currentPage}
           />
         )}
+<<<<<<< HEAD:src/app/(client)/(withHDFT)/product/[slug]/ProductReviewItem.tsx
+========
+import React, { useState } from 'react';
+import ProductReviewRating from './ProductReviewRating';
+import ProductReviewForm from './ProductReviewForm';
+import ProductReviewItem from './ProductReviewItem';
+import { useQuery } from '@tanstack/react-query';
+import { useReview } from '@/hooks/useReview';
+
+const ProductReview = ({ product }) => {
+  //State of current page for pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const onSetCurrentPage = (page) => {
+    setCurrentPage(page);
+  };
+
+  const { onGetProductReview, onGetProductReviewRating } = useReview();
+  //Get review data per page from API
+  // Define a query key and fetch function for fetching review data
+  const reviewDataQueryKey = ['productReview', product.id, currentPage];
+  const fetchReviewData = async () => {
+    const fetchedReviewData = await onGetProductReview(product.id, currentPage);
+    return fetchedReviewData;
+  };
+
+  // Define a query key and fetch function for fetching review rating data
+  const reviewRatingQueryKey = ['productReviewRating', product.id];
+  const fetchReviewRatingData = async () => {
+    const fetchedReviewRatingData = await onGetProductReviewRating(product.id);
+    return fetchedReviewRatingData;
+  };
+
+  // Fetch review data
+  const {
+    data: reviewData,
+    isFetched: isReviewDataFetched,
+    refetch: refetchReviewData,
+  } = useQuery(reviewDataQueryKey, fetchReviewData, {
+    staleTime: 1000 * 60 * 1,
+    keepPreviousData: true,
+  });
+
+  // Fetch review rating data
+  const { data: reviewRatingData, refetch: refetchReviewRatingData } = useQuery(
+    reviewRatingQueryKey,
+    fetchReviewRatingData,
+    {
+      staleTime: 1000 * 60 * 1,
+      keepPreviousData: true,
+    }
+  );
+
+  return (
+    <div>
+      <div className=" flex-col gap-1 mt-20 lg:mt-25 justify-center items-center flex text-[34px] font-semibold mb-2 leading-tight">
+        Reviews
+        <div className="w-full pt-2">
+          <ProductReviewRating reviewRatingData={reviewRatingData} />
+        </div>
+        <div className="container w-full">
+          <ProductReviewForm
+            product={product}
+            reviewRatingRefetch={refetchReviewRatingData}
+            reviewItemRefetch={refetchReviewData}
+          ></ProductReviewForm>
+        </div>
+      </div>
+      <div className="w-full py-5">
+        <ProductReviewItem
+          reviewItemData={reviewData}
+          currentPage={currentPage}
+          setCurrentPage={onSetCurrentPage}
+          isFetched={isReviewDataFetched}
+        />
+>>>>>>>> d5e50d7 (Complete ReviewForm):src/app/(client)/product/[slug]/ProductReview.tsx
+=======
+>>>>>>> d5e50d7 (Complete ReviewForm):src/app/(client)/product/[slug]/ProductReviewItem.tsx
       </div>
     </div>
   );
