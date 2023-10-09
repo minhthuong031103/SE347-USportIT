963966716ceae0a1a28600c61608dcf9a45d7d61
minhthuong031103 <nominh67@gmail.com>
2023-10-09 21:21:13 +0700

    push new

--- src/app/api/product/all/route.ts
+++ src/app/api/product/all/route.ts
@@ -3,22 +3,31 @@ import prisma from '@/lib/prisma';
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
 
-  const page = parseInt(searchParams.get('page') || '0');
-  const limit = parseInt(searchParams.get('limit') || '0');
+  const page = parseInt(searchParams.get('page') || '1');
+  const limit = parseInt(searchParams.get('limit') || '10');
+  const search = searchParams.get('search') || '';
 
-  const productCount = await prisma.product.count({});
-
-  const products = await prisma.product.findMany({
+  const all = await prisma.product.findMany({
+    skip: (page - 1) * limit,
     take: limit,
-    skip: page * limit,
+    where: {
+      name: {
+        contains: search,
+      },
+    },
   });
-
-  const response = {
-    data: [...products],
-    page,
-    totalPages: productCount / limit,
-    totalItems: productCount,
+  const total = await prisma.product.count({
+    where: {
+      name: {
+        contains: search,
+      },
+    },
+  });
+  const totalPage = Math.ceil(total / limit);
+  const data = {
+    data: all,
+    totalItems: total,
+    totalPage,
   };
-
-  return new Response(JSON.stringify(response), { status: 200 });
+  return new Response(JSON.stringify(data), { status: 200 });
 }
