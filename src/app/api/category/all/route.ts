2919eb2f0b4ed0a4951417b75e24fe9001651ef5
Đỗ Mai Minh Quân <91861930+MinhQuan020903@users.noreply.github.com>
2023-10-23 22:58:21 +0700

    fix conflict

--- src/app/api/category/all/route.ts
+++ src/app/api/category/all/route.ts
@@ -2,8 +2,8 @@ import prisma from '@/lib/prisma';
 
 export async function GET() {
   const categories = await prisma.category.findMany();
-  if (categories) {
-    return new Response(JSON.stringify(categories), { status: 200 });
+  if (categories.length > 0) {
+    return new Response(JSON.stringify({ data: categories, status: 200 }));
   } else {
     return new Response(JSON.stringify({ message: 'error' }), { status: 500 });
   }
